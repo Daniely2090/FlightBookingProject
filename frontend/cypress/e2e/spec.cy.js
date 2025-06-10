fc5c187006bc745c5cca280a65aed14ff27a5954
GAL <galwa@ac.sce.ac.il>
2025-06-11 01:11:39 +0300

    Updated Automation & CI/CD Pipelines

--- frontend/cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Homepage Tests', () => {
-  it('Should load the homepage', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Flight Booking') // <-- Replace with actual text on your homepage
-  })
-})
