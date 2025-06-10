7bccf7cc4aa8cd483bf61994f882d00ff2a46d53
GAL <galwa@ac.sce.ac.il>
2025-06-11 01:58:16 +0300

    Updated Automation & CI/CD Pipelines

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USER || 'admin',
   process.env.DATABASE_PASSWORD || 'secret',
   {
-    host: process.env.DATABASE_HOST || 'localhost',
+    host: process.env.DATABASE_HOST || 'flightbookingproject-database-1',
     dialect: 'postgres',
     port: process.env.DATABASE_PORT || 5432,
     logging: false,
